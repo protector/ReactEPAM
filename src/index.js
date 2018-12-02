393647d261d343eead17c3898825c4742f04a978
protector <zsolt.paksi@gmail.com>
2018-12-03 00:35:36 +0100

    Task2 - Setup Webpack

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'World'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>Hello me</p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
